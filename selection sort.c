da2850911548e22167a0bd1dd93282a4701189d2
vivekmakkina <112234444+vivekmakkina@users.noreply.github.com>
2023-08-29 13:08:00 +0530

    Create selection sort.c

---
+++ selection sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex;
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(&arr[i], &arr[minIndex]);
+    }
+}
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Original array: ");
+    printArray(arr, n);
+    selectionSort(arr, n);
+    printf("Sorted array: ");
+    printArray(arr, n);
+    return 0;
+}
