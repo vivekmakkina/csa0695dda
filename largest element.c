51f1dc1fb2b51f5c74497d67288dfbd114db41b2
vivekmakkina <112234444+vivekmakkina@users.noreply.github.com>
2023-08-29 10:16:11 +0530

    Create largest element.c

---
+++ largest element.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("The largest element in the array is: %d\n", largest);
+
+    return 0;
+}
