89112b58c1d86f41700f83d27061934d4859ae63
vivekmakkina <112234444+vivekmakkina@users.noreply.github.com>
2023-08-31 10:12:56 +0530

    Create linear search.c

---
+++ linear search.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+  int array[100], search, c, n;
+  printf("Enter number of elements in array\n");
+  scanf("%d", &n);
+  printf("Enter %d integer(s)\n", n);
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+  printf("Enter a number to search\n");
+  scanf("%d", &search);
+  for (c = 0; c < n; c++)
+  {
+    if (array[c]==search)  
+    {
+      printf("%d is present at location %d.\n", search, c+1);
+      break;
+    }
+  }
+}
